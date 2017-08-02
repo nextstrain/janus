80b617cc2cb4db3ae9edf4f202c36de05711fc6e
John Huddleston <huddlej@gmail.com>
2017-08-02 12:50:26 -0700

    added miniconda to path in build script and pass through all command line arguments to snakemake.

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Setup the environment.
+PATH="$HOME/miniconda3/bin:$PATH"
 source activate janus_python3
 
 # Run multiple build jobs at a time on the cluster.
-snakemake --use-conda -w 60 -j 4 --cluster-config cluster.json --cluster "sbatch --nodes=1 --ntasks=1 --mem={cluster.memory} --cpus-per-task={cluster.cores} --tmp={cluster.disk} --time={cluster.time} --job-name='{cluster.name}' --output='{cluster.stdout}' --error='{cluster.stderr}'"
+snakemake --use-conda -w 60 --cluster-config cluster.json --cluster "sbatch --nodes=1 --ntasks=1 --mem={cluster.memory} --cpus-per-task={cluster.cores} --tmp={cluster.disk} --time={cluster.time} --job-name='{cluster.name}' --output='{cluster.stdout}' --error='{cluster.stderr}'" $*
