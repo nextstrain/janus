59ee041e3d0af6021faa4d56eab8baca938e8431
John Huddleston <huddlej@gmail.com>
2017-08-02 11:53:08 -0700

    use the conda directives when running a standard build.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 source activate janus_python3
 
 # Run multiple build jobs at a time on the cluster.
-snakemake -w 60 -j 4 --cluster-config cluster.json --cluster "sbatch --nodes=1 --ntasks=1 --mem={cluster.memory} --cpus-per-task={cluster.cores} --tmp={cluster.disk} --time={cluster.time} --job-name='{cluster.name}' --output='{cluster.stdout}' --error='{cluster.stderr}'"
+snakemake --use-conda -w 60 -j 4 --cluster-config cluster.json --cluster "sbatch --nodes=1 --ntasks=1 --mem={cluster.memory} --cpus-per-task={cluster.cores} --tmp={cluster.disk} --time={cluster.time} --job-name='{cluster.name}' --output='{cluster.stdout}' --error='{cluster.stderr}'"
